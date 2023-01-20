f76068cf41fecd8b0de33bcf7332b37c9a3c8ba4
GraceCS <beicaoipht@gmail.com>
2023-01-20 17:41:37 -0500

    Add a graph html and update on Streamlit Clound

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import streamlit as st
 from PIL import Image
 import networkx as nx
 from pyvis.network import Network
+import streamlit.components.v1 as components
 
 header = st.container()
 dataset = st.container()
 features = st.container()
+graph_visu = st.container()
 model_training = st.container()
 
 
@@ -42,6 +44,19 @@ with features:
     st.header("Graph features")
     st.markdown('* **1st dimension:** wavelength')
     st.markdown('* **2nd dimension:** wavelength')
+
+with graph_visu:
+    st.header("Pyvis Interactive Graphs")  
+    try:  # Save and read graph as HTML file (on Streamlit Sharing)
+        path = '/tmp'
+        HtmlFile = open(f'{path}/net0_knn.html', 'r', encoding='utf-8')
+    except:   # Save and read graph as HTML file (locally)
+        path = 'html_files'
+        HtmlFile = open(f'{path}/net0_knn.html', 'r', encoding='utf-8')
+
+    # Load HTML file in HTML component for display on Streamlit page
+    components.html(HtmlFile.read(), height=435)
+
 
 with model_training:
     st.header('visualize the layer in GAT')
